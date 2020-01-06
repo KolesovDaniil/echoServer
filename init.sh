7acf4a4d6543b6d31b57e8c04dce2c50fc2831b1
KolesovDaniil <kolesov.daniil39@gmail.com>
2020-01-06 21:06:41 +0300

    add gunicorn restart

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo gunicorn -b 0.0.0.0:80 hello
+sudo /etc/init.d/gunicorn stop
+sudo gunicorn -b 0.0.0.0:8080 hello
