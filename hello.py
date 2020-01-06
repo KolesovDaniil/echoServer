947f4f2e5bcdd9fd8729dcdb6f836ef8cd093456
KolesovDaniil <kolesov.daniil39@gmail.com>
2020-01-06 21:28:57 +0300

    Content TTTTType

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def application(environ, start_response):
-    start_response('200 OK', [('Content-type', 'text/plain')])
+    start_response('200 OK', [('Content-Type', 'text/plain')])
     body = [bytes(i + '\n', 'utf-8') for i in environ['QUERY_STRING'].split('&')]
     return body
