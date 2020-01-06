ebc4757c76dbab6949d529a5ae7e758675d4ad7f
KolesovDaniil <kolesov.daniil39@gmail.com>
2020-01-06 21:58:47 +0300

    links changes #2

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-sudo rm /etc/nginx/sites-available/default
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-available/default
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicorn stop
 sudo gunicorn -b 0.0.0.0:8080 hello
