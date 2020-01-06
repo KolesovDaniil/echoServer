adf13acfe3187f95b824365c81387d6f76ba94bf
KolesovDaniil <kolesov.daniil39@gmail.com>
2020-01-06 20:29:56 +0300

    add init script

--- wsgi.py
+++
@@ -1,4 +0,0 @@
-def application(environ, start_response):
-    start_response('200 OK', [('Content-type', 'text/html')])
-    body = [i + '\n' for i in environ['QUERY_STRING'].split('&')]
-    return body
