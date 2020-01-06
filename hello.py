15918aa179fee2d065e78daae8683178d25a4a90
KolesovDaniil <kolesov.daniil39@gmail.com>
2020-01-06 21:20:15 +0300

    change content type

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def application(environ, start_response):
-    start_response('200 OK', [('Content-type', 'text/html')])
+    start_response('200 OK', [('Content-type', 'text/plain')])
     body = [bytes(i + '\n', 'utf-8') for i in environ['QUERY_STRING'].split('&')]
     return body
