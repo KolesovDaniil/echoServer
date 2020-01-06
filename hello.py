6db050af32ee1f073c4dfdceefbaf585141c1103
KolesovDaniil <kolesov.daniil39@gmail.com>
2020-01-06 21:32:19 +0300

    from utf-8 to ascii

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    body = [bytes(i + '\n', 'utf-8') for i in environ['QUERY_STRING'].split('&')]
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     return body
