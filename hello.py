37664ec0c4221e5f1e282c433ec5b9d20742c6bb
KolesovDaniil <kolesov.daniil39@gmail.com>
2020-01-07 01:02:36 +0300

    encode fix

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    body = [i + '\n' for i in environ['QUERY_STRING'].split('&')]
+    body = [i.encode('ascii') for i in body]
     return body
