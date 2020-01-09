d7d7ac9188f0d43df50c9bb74ba2a57e3116d87a
KolesovDaniil <kolesov.daniil39@gmail.com>
2020-01-09 16:31:26 +0300

    another try

--- test.py
+++
@@ -1,5 +0,0 @@
-def application(env, s_r):
-	s_r('200 OK', [('Content-Type', 'text/plain')])
-	print(env['QUERY_STRING'] == '')
-	print(env['QUERY_STRING'])
-	return [env['QUERY_STRING'].replace('&','\n')]
