771d784f7089fe1c4922a9491dc3ce30c46130bd
KolesovDaniil <kolesov.daniil39@gmail.com>
2020-01-14 19:41:35 +0300

    make an package from app and add gunicorn start

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
-sudo ln -s $(pwd)/etc/config/nginx.conf /etc/nginx/conf.d/test.conf
+sudo ln -s $(pwd)/config/nginx.conf /etc/nginx/conf.d/test.conf
 sudo service nginx restart
+gunicorn -b 127.0.0.1:8080 app.hello
